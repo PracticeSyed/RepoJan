5466e92acf94fa66d8792019fd77fcee78485534
Syedpractice <syed16riyaz@gmail.com>
2024-01-19 16:40:08 +0530

    Commited the code

---
+++ src/org/sample/SampleClassGit.java
@@ -0,0 +1,18 @@
+package org.sample;
+
+public class SampleClassGit {
+	
+	
+	static void methGit1() {
+		
+		System.out.println("Hello world");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		methGit1();
+		
+	}
+
+}
